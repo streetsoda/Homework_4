2f8ed3bf2706834641454c25c5498b79dec7a2b1
streetsoda <34893003+streetsoda@users.noreply.github.com>
2019-02-11 16:51:21 +0200

    Add files via upload

---
+++ rhombus.php
@@ -0,0 +1,19 @@
+﻿<?php 
+
+function printRhombus ($height) {
+    $width = $height;
+	
+    for ($i = 1; $i <= $height; $i += 2) {
+        echo str_repeat('  ', --$width), str_repeat(' #', $i), PHP_EOL;
+    }
+	
+    for ($k = $height-=2; $k >= 0; $k -= 2) {
+	    echo str_repeat('  ', ++$width), str_repeat(' #', $k), PHP_EOL;
+    }
+}
+
+if($argv[1] <= 0 || $argv[1] % 2 == 0 || $argv[1] == NULL) {
+    echo 'Please enter odd number that is greater than 0';
+} else {
+printRhombus($argv[1]);
+}	
---
+++ snake_array.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+function snakeArray($side) {
+    $size = $side*$side;
+    $arr = range(1, $size);
+    $reverse = array_chunk($arr,$side);
+	
+    foreach ($reverse as $key =>& $value) {
+        if ($key % 2 == 1) {
+            $value = array_reverse($value);
+        }
+    }
+    return $reverse;
+}
+function printArray($array) {
+    foreach($array as $val) {
+        foreach ($val as $number) {
+            echo "$number ";
+        }
+        echo PHP_EOL;
+    }
+}
+
+if(is_numeric($argv[1]) && $argv[1] >= 1 && $argv[1]%1 == 0) {
+    printArray(snakeArray((int)$argv[1]));
+} else {
+    echo 'Please enter a valid number that is greater than 0';
+}
