79861545afa1f1477ed3471fb6ac65f1c676c3a3
streetsoda <34893003+streetsoda@users.noreply.github.com>
2019-02-11 16:52:25 +0200

    Update rhombus.php

--- rhombus.php
+++ rhombus.php
@@ -8,12 +8,12 @@ function printRhombus ($height) {
     }
 	
     for ($k = $height-=2; $k >= 0; $k -= 2) {
-	    echo str_repeat('  ', ++$width), str_repeat(' #', $k), PHP_EOL;
+        echo str_repeat('  ', ++$width), str_repeat(' #', $k), PHP_EOL;
     }
 }
 
 if($argv[1] <= 0 || $argv[1] % 2 == 0 || $argv[1] == NULL) {
     echo 'Please enter odd number that is greater than 0';
 } else {
-printRhombus($argv[1]);
+    printRhombus($argv[1]);
 }	
